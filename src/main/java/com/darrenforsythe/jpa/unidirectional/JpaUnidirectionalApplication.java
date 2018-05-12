eec6df3c50aee13438e19a4c30bfe871bed501c5
Darren Forsythe <darren.forsythe@liberty-it.co.uk>
2018-05-12 16:10:41 +0100

    initial project

---
+++ src/main/java/com/darrenforsythe/jpa/unidirectional/JpaUnidirectionalApplication.java
@@ -0,0 +1,12 @@
+package com.darrenforsythe.jpa.unidirectional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaUnidirectionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaUnidirectionalApplication.class, args);
+	}
+}
